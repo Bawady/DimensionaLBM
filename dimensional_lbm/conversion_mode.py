46a7479ab5d5c2e99aae61d239a27c9abbfec094
Dylan Baumann <dylan.baumann@tuwien.ac.at>
2026-03-12 11:04:32 +0100

    improve formatting

--- dimensional_lbm/conversion_mode.py
+++ dimensional_lbm/conversion_mode.py
@@ -1,17 +1,13 @@
 from typing import TypeVar
 
-
 class Dimensional:
 	pass
-
 
 class NonDimensional:
 	pass
 
-
 class MagnitudeOnly:
 	pass
-
 
 type ConversionMode = Dimensional | NonDimensional | MagnitudeOnly
 
